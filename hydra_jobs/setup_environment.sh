e5f595e5d99191c50a325eb9013f039ec949454a
e-koch <koch.eric.w@gmail.com>
2024-06-12 16:14:38 -0400

    Small updates for reproducing environment

--- hydra_jobs/setup_environment.sh
+++ hydra_jobs/setup_environment.sh
@@ -2,3 +2,10 @@
 
 conda env create -f zooniverse_environment.yml
 
+conda actiavte bubblezoo
+
+# Grab the dev version of reproject
+git clone https://github.com/astropy/reproject.git
+cd python-reprojection
+pip install -e .
+
