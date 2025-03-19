aead8a11bc4e8d97491681eef805c7990a7583bf
Eric Koch <koch.eric.w@gmail.com>
2025-03-19 07:44:12 -0400

    Add check for failure cases for negentropy stretch

--- max_entropy_stretch.py
+++ max_entropy_stretch.py
@@ -12,12 +12,26 @@ def image_entropy(pars, image):
     # minimize negative entropy = maximize entropy
     return(negentropy)
 
-def maxent_bounds(image, return_norm=False):
+def maxent_bounds(image, return_norm=False,
+                  raise_error_on_fail=False):
+
+    min_val = np.nanmin(image)
+    max_val = np.nanmax(image)
+
+    fit_fail = np.all(np.isnan(image)) or min_val == max_val
+
+    if fit_fail:
+        if raise_error_on_fail:
+            raise ValueError('Image has no valid data or range.')
+        else:
+            return np.nanmin(image), np.nanmax(image)
+
     params = Parameters()
-    params.add('vmin', value=np.nanpercentile(image, 1), vary=False, min=np.nanmin(image))
-    params.add('range', value=(np.nanmax(image)-np.nanmin(image))/2,
-               vary=True, min=0, max=(np.nanmax(image)-np.nanmin(image)))
+    params.add('vmin', value=np.nanpercentile(image, 1), vary=False, min=min_val)
+    params.add('range', value=(max_val-min_val)/2,
+               vary=True, min=0, max=(max_val-min_val))
     params.add('vmax', expr='vmin + range')
+
     mm = Minimizer(image_entropy, params, fcn_args=(image,), jac='3-point')
     result = mm.minimize(method='lbsfgb')
 
